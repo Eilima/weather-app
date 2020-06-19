bd547b0cccbf5d8186c37d8aed70018d2d1248fa
Eilima <ericklima34@gmail.com>
2020-06-19 18:28:40 -0500

    Add Submit button and class name

--- src/Components/Display.js
+++ src/Components/Display.js
@@ -4,13 +4,16 @@ export class Display extends React.Component {
     render(){
         return (
             <div>
-                <input className='inputField' placeholder='City' type='text'></input>
-                <input className='inputField' placeholder='Country' type='text'></input>
-
-                <p className='weather-info'>Location:</p>
-                <p className='weather-info'>Temperature:</p>
-                <p className='weather-info'>Humidity:</p>
-                <p className='weather-info'>Conditions:</p>
+                <div className='infoBox' >
+                    <input className='inputField' placeholder='City' type='text'></input>
+                    <input className='inputField' placeholder='Country' type='text'></input>
+                    <button>Submit</button>
+                    
+                    <p className='weather-info'>Location:</p>
+                    <p className='weather-info'>Temperature:</p>
+                    <p className='weather-info'>Humidity:</p>
+                    <p className='weather-info'>Conditions:</p>
+                </div>
             </div>
         )
     }
