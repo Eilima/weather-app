e70bd525e65fa8124f6ebb72e0f883cf664a7e44
Eilima <ericklima34@gmail.com>
2020-06-20 21:55:21 -0500

    Add the Search method and form tags

--- src/Components/Display.js
+++ src/Components/Display.js
@@ -1,18 +1,40 @@
 import React from 'react'
 
 export class Display extends React.Component {
+    state = {
+        location: 'test',
+        temperature: null,
+        humidity: null,
+        conditions: null,
+    }
+    
+    search = async () => {
+        let cityName = 'Houston'
+        let apiKey = ''
+        const api_call = await fetch(`api.openweathermap.org/data/2.5/weather?q=Houston&units=metric&appid=${apiKey}`)
+        console.log(api_call)
+        console.log(typeof api_call)
+        const data = await api_call.json()
+        console.log(data)
+    }
+
+    submit = (e) => {
+        e.preventDefault();
+        this.search()
+    }
     render(){
         return (
             <div>
                 <div className='infoBox' >
-                    <input className='inputField' placeholder='City' type='text'></input>
-                    <input className='inputField' placeholder='Country' type='text'></input>
-                    <button>Submit</button>
-                    
-                    <p className='weather-info'>Location:</p>
-                    <p className='weather-info'>Temperature:</p>
-                    <p className='weather-info'>Humidity:</p>
-                    <p className='weather-info'>Conditions:</p>
+                    <form>
+                        <input className='inputField' placeholder='City' type='text'></input>
+                        <input className='inputField' placeholder='Country' type='text'></input>
+                        <button onClick={this.submit} >Submit</button>
+                    </form>
+                    <p className='weather-info'>Location: {this.state.location} </p>
+                    <p className='weather-info'>Temperature: {this.state.temperature} </p>
+                    <p className='weather-info'>Humidity: {this.state.humidity} </p>
+                    <p className='weather-info'>Conditions: {this.state.conditions} </p>
                 </div>
             </div>
         )
