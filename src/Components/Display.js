d2af45e13cd63b4513dc0f3f9ca1cdabbf3d97bf
Eilima <ericklima34@gmail.com>
2020-06-22 12:50:49 -0500

    Add api call and change the input field to edit state on change

--- src/Components/Display.js
+++ src/Components/Display.js
@@ -1,44 +1,43 @@
 import React from 'react'
+import {search} from '../OpenWeather'
 
 export class Display extends React.Component {
     state = {
-        location: 'test',
-        temperature: 'test',
-        humidity: 'test',
-        conditions: 'test',
-    }
-    
-    search = async () => {
-        let cityName = 'Houston'
-        let apiKey = ''
-        const api_call = await fetch(`api.openweathermap.org/data/2.5/weather?q=Houston&units=metric&appid=${apiKey}`)
-        console.log(api_call)
-        console.log(typeof api_call)
-        const data = await api_call.json()
-        console.log(data)
+        location: null,
+        temperature: null,
+        humidity: null,
+        cityName: null,
     }
 
-    submit = (e) => {
-        e.preventDefault();
-        this.search()
+    onClick = async (e) =>{
+        e.preventDefault()
+        let tet = e.target.value
+        const apiKey = '3515eb54a10f2ef0d46d3777bab42cae'
+        let units = 'metric'
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.state.cityName}&units=${units}&appid=${apiKey}`)
+        const data = await response.json()
+        this.setState({
+            location: data.name,
+            temperature: data.main.temp,
+            humidity: data.main.humidity,
+        })
     }
+
+
     render(){
         return (
             <div>
                 <div className='infoBox' >
                     <form>
-                        <input className='inputField' placeholder='City' type='text'></input>
-                        <input className='inputField' placeholder='Country' type='text'></input>
-                        <button onClick={this.submit} >Submit</button>
+                        <input className='inputField' placeholder='City' type='text' onChange={e => this.setState({cityName: e.target.value})} ></input>
+                        <button onClick={this.onClick} >Submit</button>
                     </form>
                     <p className='weather-info'>Location</p>
                     <p className='weather-info'>{this.state.location}</p>
                     <p className='weather-info'>Temperature</p>
                     <p className='weather-info'>{this.state.temperature}</p>
                     <p className='weather-info'>Humidity</p>
-                    <p className='weather-info'>{this.state.humidity}</p>
-                    <p className='weather-info'>Conditions</p>
-                    <p className='weather-info'>{this.state.conditions}</p>
+                    <p className='weather-info'>{this.state.humidity === null ? '' : `${this.state.humidity}%`}</p>
                 </div>
             </div>
         )
