52ba4d449e00ac7098889df9850f5bd1333a8bc3
Eilima <ericklima34@gmail.com>
2020-06-22 13:55:35 -0500

    Add the ability to change unit type farenheit and celsius

--- src/Components/Display.js
+++ src/Components/Display.js
@@ -7,19 +7,29 @@ export class Display extends React.Component {
         temperature: null,
         humidity: null,
         cityName: null,
+        unitValue: 'metric'
     }
 
     onClick = async (e) =>{
         e.preventDefault()
         let tet = e.target.value
         const apiKey = '3515eb54a10f2ef0d46d3777bab42cae'
-        let units = 'metric'
-        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.state.cityName}&units=${units}&appid=${apiKey}`)
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.state.cityName}&units=${this.state.unitValue}&appid=${apiKey}`)
         const data = await response.json()
         this.setState({
             location: data.name,
             temperature: data.main.temp,
             humidity: data.main.humidity,
+        })
+    }
+
+    changeUnit = () => {
+        this.state.unitValue === 'metric' ? 
+        this.setState({
+            unitValue: 'imperial' 
+        })
+        : this.setState({
+            unitValue: 'metric'
         })
     }
 
@@ -29,14 +39,20 @@ export class Display extends React.Component {
             <div>
                 <div className='infoBox' >
                     <form>
-                        <input className='inputField' placeholder='City' type='text' onChange={e => this.setState({cityName: e.target.value})} ></input>
-                        <button onClick={this.onClick} >Submit</button>
+                        <div className='weather-header' >
+                            <input className='inputField' placeholder='City' type='text' onChange={e => this.setState({cityName: e.target.value})} ></input>
+                            <button onClick={this.onClick}><i class="material-icons">search</i></button>
+                        </div>    
+                        <label className='switch'>
+                            <input onChange={this.changeUnit} type='checkbox'/>
+                            <span className='slider round'></span>
+                        </label>
                     </form>
-                    <p className='weather-info'>Location</p>
+                    <h1 className='weather-info'>Location</h1>
                     <p className='weather-info'>{this.state.location}</p>
-                    <p className='weather-info'>Temperature</p>
-                    <p className='weather-info'>{this.state.temperature}</p>
-                    <p className='weather-info'>Humidity</p>
+                    <h1 className='weather-info'>Temperature</h1>
+                    <p className='weather-info'>{this.state.temperature === null ? '' : `${this.state.temperature}°${this.state.unitValue === 'metric' ? 'C' : 'F'}`} </p>
+                    <h1 className='weather-info'>Humidity</h1>
                     <p className='weather-info'>{this.state.humidity === null ? '' : `${this.state.humidity}%`}</p>
                 </div>
             </div>
