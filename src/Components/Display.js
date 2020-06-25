87960369227b2ac6d057e418464298af7f317321
Eilima <ericklima34@gmail.com>
2020-06-25 12:49:35 -0500

    Fix the ability to change temperatures when api has already been called

--- src/Components/Display.js
+++ src/Components/Display.js
@@ -7,39 +7,49 @@ export class Display extends React.Component {
         temperature: null,
         humidity: null,
         cityName: null,
-        unitValue: 'metric'
+        unitValue: 'metric',
+        description: null
     }
 
     apiCall = async () => {
             const apiKey = '3515eb54a10f2ef0d46d3777bab42cae'
             const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.state.cityName}&units=${this.state.unitValue}&appid=${apiKey}`)
             const data = await response.json()
-            this.setState({
-                location: data.name,
-                temperature: data.main.temp,
-                humidity: data.main.humidity,
-            })
+                this.setState({
+                    location: data.name,
+                    temperature: data.main.temp,
+                    humidity: data.main.humidity,
+                })
     }
 
-    onClick =  (e) => {
+    onClick = (e) => {
         e.preventDefault()
         this.apiCall()
         
     }
 
     changeUnit = () => {
-        this.state.unitValue === 'metric' ? 
-        this.setState({
-            unitValue: 'imperial' 
-        })
-        : this.setState({
-            unitValue: 'metric'
-        })
-        
-        if (this.state.cityName !== null)
+        this.state.unitValue === 'metric' ? this.setState({unitValue: 'imperial'}) : this.setState({unitValue: 'metric'})
+    }
+
+    switchFlip = (e) => {
+        if (this.state.temperature === null)
+            this.changeUnit()
+            
+        let originalTemp = this.state.temperature
+
+        if (this.state.unitValue === 'imperial' && this.state.temperature !== null) {
+            this.changeUnit()
+            this.setState({
+                temperature: ((this.state.temperature - 32) * (5/9)).toFixed(2)
+            })
+
+        } else if (this.state.temperature !== null && this.state.unitValue !== 'imperial'){
+            this.changeUnit()
             this.setState({
                 temperature: ((this.state.temperature * 9/5) + 32).toFixed(2)
             })
+        }
     }
 
     metricOrFaren = () => {
@@ -52,17 +62,17 @@ export class Display extends React.Component {
     // (0°C × 9/5) + 32 = 32°F
 
 
-    render(){
+    render() { 
         return (
             <div>
                 <div className='infoBox' >
                     <form>
                         <div className='weather-header' >
                             <input className='inputField' placeholder='City' type='text' onChange={e => this.setState({cityName: e.target.value})} ></input>
-                            <button onClick={this.onClick}><i class="material-icons">search</i></button>
+                            <button onClick={this.onClick}><i className="material-icons">search</i></button>
                         </div>    
                         <label className='switch'>
-                            <input onChange={this.changeUnit} type='checkbox'/>
+                            <input onChange={e => this.switchFlip(e)} type='checkbox'/>
                             <span className='slider round'></span>
                         </label>
                     </form>
