8c69801c737fd9620a2db465d8583dba008a60e5
Eilima <ericklima34@gmail.com>
2020-07-08 11:23:20 -0500

    Delete the call to a non existent function/file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import './App.css';
-import {search} from './OpenWeather'
 import { Display } from './Components/Display';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Display search={search} />
+        <Display/>
       </header>
     </div>
   );
